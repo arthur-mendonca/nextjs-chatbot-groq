1ef9da608085b6b5669a9ea65c06fdc061655071
arthur-mendonca <arthu91@gmail.com>
2024-08-05 11:24:56 -0300

    fix: estilo da página start

--- app/(intern)/start/page.tsx
+++ app/(intern)/start/page.tsx
@@ -13,7 +13,7 @@ const Introduction = () => {
   return (
     <Layout>
       <div className="min-h-[80vh] h-full flex flex-col items-center justify-center p-4">
-        <h1 className="text-4xl font-bold mb-6">
+        <h1 className="text-4xl font-bold mb-6 text-center">
           Bem-vindo ao Study Roadmap Chatbot
         </h1>
         <p className="text-lg mb-4 text-center">
