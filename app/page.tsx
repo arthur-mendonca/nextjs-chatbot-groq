bec50d4a2c63b335a4317334368bc5d3c59cf51d
arthur-mendonca <arthu91@gmail.com>
2024-08-05 00:33:07 -0300

    fix: consertar erro em page.tsx - deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,24 @@
+"use client";
 import Layout from "./components/Layout/Layout";
+import { useRouter } from "next/navigation";
 
-import React, { ReactNode } from "react";
+export default function Home() {
+  const router = useRouter();
 
-interface HomeProps {
-  children: React.ReactNode;
-}
-
-export default function Home(props: HomeProps) {
+  const handleStart = () => {
+    router.push("/start");
+  };
   return (
     <>
-      <Layout>{props.children}</Layout>
+      <Layout>
+        <div className="flex w-full items-center justify-center h-[75vh]">
+          <button
+            onClick={handleStart}
+            className="px-6 py-3 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-300">
+            Iniciar
+          </button>
+        </div>
+      </Layout>
     </>
   );
 }
