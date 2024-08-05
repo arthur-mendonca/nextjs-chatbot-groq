4c051b1ea1b4bedd54b47bc17239968eb6283488
arthur-mendonca <arthu91@gmail.com>
2024-08-05 00:20:56 -0300

    fix: erro em app/page.tsx consertado para deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import Chatbot from "./components/Chatbot/Chatbot";
 import Layout from "./components/Layout/Layout";
 
 import React, { ReactNode } from "react";
 
 interface HomeProps {
-  children: ReactNode;
+  children: React.ReactNode;
 }
 
 export default function Home({ children }: HomeProps): JSX.Element {
