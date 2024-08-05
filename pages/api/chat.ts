ccd44f9f9a107fa406005bcfd623fb1e71d9701f
arthur-mendonca <arthu91@gmail.com>
2024-08-05 09:24:12 -0300

    fix: atualizar ordens para o Chatbot

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -9,7 +9,7 @@ const client = new Groq({ apiKey: groq_api_key });
 export const systemPrompt: Message = {
   role: "system",
   content: `Você é um ajudante virtual cujo papel é receber a descrição de uma vaga de tecnologia e 
-    fornecer um detalhes sobre as tecnologias exigidas pela vaga, bem como elaborar um plano de estudos de acordo com 
+    fornecer detalhes sobre as tecnologias exigidas pela vaga, bem como elaborar um plano de estudos de acordo com 
     as exigências da vaga. Suas respostas devem ser as mais detalhadas e úteis possíveis.`,
 };
 
@@ -24,6 +24,7 @@ export default async function handler(
   }
 
   const { chat_history, user_input } = req.body;
+
   const updatedChatHistory = [
     ...chat_history,
     { role: systemPrompt.role, content: systemPrompt.content },
@@ -34,14 +35,17 @@ export default async function handler(
     const response = await client.chat.completions.create({
       model: "llama3-70b-8192",
       messages: updatedChatHistory,
-      max_tokens: 5000,
+      max_tokens: 7000,
       temperature: 0.5,
     });
+
     const assistantMessage = {
       role: "assistant",
       content: response.choices[0].message.content,
     };
+
     updatedChatHistory.push(assistantMessage);
+
     res
       .status(200)
       .json({ response: assistantMessage, chat_history: updatedChatHistory });
